f49398a16feeab549b113bc1658e342ed22b01fa
Jason Mitchell <jasonmitchell@live.co.uk>
2021-03-25 17:52:07 +0000

    Create App component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import UserStoryMap from 'UserStoryMap';
+
+function App() {
+  return (
+    <UserStoryMap></UserStoryMap>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import {ThemeProvider} from 'styled-components';
 import reportWebVitals from 'reportWebVitals';
 import GlobalStyle from 'global-style';
 import {defaultTheme} from 'themes';
-import UserStoryMap from 'UserStoryMap';
+import App from 'App';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <UserStoryMap />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
