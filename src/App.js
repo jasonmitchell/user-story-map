4b4b949c1e6c3566ed90266c159674e6e5fd17ed
Jason Mitchell <jasonmitchell@live.co.uk>
2021-03-28 21:01:25 +0100

    Add header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import UserStoryMap from 'UserStoryMap';
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  max-height: 100vh;
+`;
+
+const Header = styled.header`
+  background: #cccccc;
+`;
+
+const Content = styled.section`
+  flex: 1;
+  overflow: auto;
+  padding-top: 0.5em;
+`;
 
 function loadState() {
   const state = JSON.parse(localStorage.getItem('map'));
@@ -14,9 +33,16 @@ function App() {
   const initialState = loadState();
 
   return (
-    <UserStoryMap
-      map={initialState}
-      onMapUpdated={saveState}/>
+    <Layout>
+      <Header>
+        User Story Map
+      </Header>
+      <Content>
+        <UserStoryMap
+          map={initialState}
+          onMapUpdated={saveState}/>
+      </Content>
+    </Layout>
   )
 }
 
