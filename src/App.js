be6dbe55bf1cb319e019aa3d6f23dbbeab57a54c
Jason Mitchell <jasonmitchell@live.co.uk>
2021-03-29 21:07:32 +0100

    Make horizontal dividers span width of page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import UserStoryMap from 'views/UserStoryMap';
 const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100vw;
+  min-width: 100vw;
   height: 100vh;
   max-height: 100vh;
 `;
