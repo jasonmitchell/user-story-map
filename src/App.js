956a57c1ea2446805d49b9a094732631030208c6
Jason Mitchell <jasonmitchell@live.co.uk>
2021-05-16 21:50:15 +0100

    Add editable map title

--- src/App.js
+++ src/App.js
@@ -16,6 +16,23 @@ const Header = styled.header`
   color: ${props => props.theme.accentText};
 `;
 
+const MapTitle = styled.input.attrs(props => ({
+  type: 'text',
+  value: props.title,
+  placeholder: props.placeholder,
+  maxLength: 50
+}))`
+  border: none;
+  outline: none;
+  font-size: 1em;
+  background: none;
+  color: #ffffff;
+
+  &::placeholder {
+    font-style: italic;
+  }
+`;
+
 const Content = styled.section`
   flex: 1;
   overflow: auto;
@@ -36,10 +53,17 @@ function clearLocalStorage() {
   localStorage.removeItem('map');
 }
 
+function updateTitle(title) {
+  let state = loadFromLocalStorage();
+  state.title = title;
+  saveToLocalStorage(state);
+}
+
 let initialState = loadFromLocalStorage();
 
 function App() {
   const [key, setKey] = useState(generateId());
+  const [title, setTitle] = useState(initialState.title);
 
   function resetUserStoryMap() {
     clearLocalStorage();
@@ -51,8 +75,13 @@ function App() {
   return (
     <Layout>
       <Header>
-        User Story Map
         <button type="button" onClick={resetUserStoryMap}>Clear</button>
+        <MapTitle title={title}
+                  placeholder="My Map"
+                  onChange={e => {
+                    updateTitle(e.target.value);
+                    setTitle(e.target.value);
+                  }} />
       </Header>
       <Content>
         <UserStoryMap
