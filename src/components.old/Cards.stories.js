a4baca4ffec9aecc59f84b4c5784e0362cd92c08
Jason Mitchell <jasonmitchell@live.co.uk>
2021-11-23 22:16:23 +0000

    Disable old stories

--- src/components.old/Cards.stories.js
+++ src/components.old/Cards.stories.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import { ActivityCard, TaskCard, StoryCard } from './Cards';
+// import React from 'react';
+// import { ActivityCard, TaskCard, StoryCard } from './Cards';
 
-export default {
-  title: 'User Story Map (Old)/Cards',
-  args: {
-    dispatch: () => {},
-    onClick: () => {}
-  },
-  parameters: {
-    actions: {
-      handles: ['click article'],
-    },
-  }
-};
+// export default {
+//   title: 'User Story Map (Old)/Cards',
+//   args: {
+//     dispatch: () => {},
+//     onClick: () => {}
+//   },
+//   parameters: {
+//     actions: {
+//       handles: ['click article'],
+//     },
+//   }
+// };
 
-const ActivityTemplate = (args) => <ActivityCard {...args} />;
-export const Activity = ActivityTemplate.bind({});
-Activity.args = {
-  isSelected: false,
-  title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-};
+// const ActivityTemplate = (args) => <ActivityCard {...args} />;
+// export const Activity = ActivityTemplate.bind({});
+// Activity.args = {
+//   isSelected: false,
+//   title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+// };
 
-const TaskTemplate = (args) => <TaskCard {...args} />
-export const Task = TaskTemplate.bind({});
-Task.args = {
-  isSelected: false,
-  title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-};
+// const TaskTemplate = (args) => <TaskCard {...args} />
+// export const Task = TaskTemplate.bind({});
+// Task.args = {
+//   isSelected: false,
+//   title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+// };
 
-const StoryTemplate = (args) => <StoryCard {...args} />
-export const Story = StoryTemplate.bind({});
-Story.args = {
-  isSelected: false,
-  title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-};
+// const StoryTemplate = (args) => <StoryCard {...args} />
+// export const Story = StoryTemplate.bind({});
+// Story.args = {
+//   isSelected: false,
+//   title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+// };
 
-const ToolbarTemplate = (args) => (
-  <div>
-    <StoryCard {...args} />
-    <StoryCard title="Sed lacinia dolor ac lacus vehicula imperdiet." isSelected={false} />
-  </div>
-);
-export const Toolbar = ToolbarTemplate.bind({});
-Toolbar.args = {
-  isSelected: true,
-  title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-}
+// const ToolbarTemplate = (args) => (
+//   <div>
+//     <StoryCard {...args} />
+//     <StoryCard title="Sed lacinia dolor ac lacus vehicula imperdiet." isSelected={false} />
+//   </div>
+// );
+// export const Toolbar = ToolbarTemplate.bind({});
+// Toolbar.args = {
+//   isSelected: true,
+//   title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+// }
