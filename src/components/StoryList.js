ae8f7766d3968a903a0c83f712ae12acb9a2c96e
Jason Mitchell <jasonmitchell@live.co.uk>
2021-11-25 21:14:49 +0000

    Story list styling

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import { StoryCard } from './Cards';
+
+const List = styled.div`
+  width: 300px;
+  margin: 0em 1em;
+`;
+
+const CardContainer = styled.div`
+  margin-bottom: 0.5em;
+`;
 
 export default function StoryList({stories}) {
   return (
-    <div>
+    <List>
       {stories.map((story) => (
-        <StoryCard key={story.id} {...story} />
+        <CardContainer key={story.id}>
+          <StoryCard {...story} />
+        </CardContainer>
       ))}
-    </div>
+    </List>
   );
 }
